5eeba52194bf9eec32de72fa93b5b219aa1c693c
Ivan García García <ivan.garcia@altia.es>
2023-03-17 10:53:50 +0100

    lists bugs fixed

--- ggbowling-back/GGBowling-Backend/GGBowling/infrastructure/src/main/java/com/ivang/GGBowling/entity/ReservaEntity.java
+++ ggbowling-back/GGBowling-Backend/GGBowling/infrastructure/src/main/java/com/ivang/GGBowling/entity/ReservaEntity.java
@@ -4,10 +4,10 @@ import jakarta.persistence.*;
 import lombok.*;
 
 import java.util.Date;
+import java.util.HashSet;
 import java.util.Set;
 @Entity
 @AllArgsConstructor
-@Data
 @NoArgsConstructor
 @Table(name = "reserva")
 public class ReservaEntity {
@@ -21,10 +21,10 @@ public class ReservaEntity {
   @ManyToOne(fetch = FetchType.LAZY)
   private UsuarioEntity usuario;
 
-  @OneToMany(mappedBy = "reserva",cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "reserva",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private Set<PistaEntity> pistas;
 
-  @OneToMany(mappedBy = "reserva",cascade = CascadeType.ALL)
+  @OneToMany(mappedBy = "reserva",cascade = CascadeType.ALL, fetch = FetchType.LAZY)
   private Set<MesaEntity> mesas;
 
   @Column(name = "fecha_hora_inicio")
@@ -42,5 +42,73 @@ public class ReservaEntity {
   @Column(name = "precio_persona")
   private Float precioPersona;
 
+  public Set<MesaEntity> getMesas() {
+    if (this.mesas == null) {
+      this.mesas = new HashSet<>();
+    }
+    return this.mesas;
+  }
 
+  public Set<PistaEntity> getPistas() {
+    if (this.pistas == null) {
+      this.pistas = new HashSet<>();
+    }
+    return this.pistas;
+  }
+
+  public Integer getReservaId() {
+    return reservaId;
+  }
+
+  public void setReservaId(Integer reservaId) {
+    this.reservaId = reservaId;
+  }
+
+  public UsuarioEntity getUsuario() {
+    return usuario;
+  }
+
+  public void setUsuario(UsuarioEntity usuario) {
+    this.usuario = usuario;
+  }
+
+  public Date getHoraEntrada() {
+    return horaEntrada;
+  }
+
+  public void setHoraEntrada(Date horaEntrada) {
+    this.horaEntrada = horaEntrada;
+  }
+
+  public Date getHoraSalida() {
+    return horaSalida;
+  }
+
+  public void setHoraSalida(Date horaSalida) {
+    this.horaSalida = horaSalida;
+  }
+
+  public Integer getPersonas() {
+    return personas;
+  }
+
+  public void setPersonas(Integer personas) {
+    this.personas = personas;
+  }
+
+  public Integer getPartidas() {
+    return partidas;
+  }
+
+  public void setPartidas(Integer partidas) {
+    this.partidas = partidas;
+  }
+
+  public Float getPrecioPersona() {
+    return precioPersona;
+  }
+
+  public void setPrecioPersona(Float precioPersona) {
+    this.precioPersona = precioPersona;
+  }
 }
