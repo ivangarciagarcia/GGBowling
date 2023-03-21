5255f7249fcef99b7508cf5b1cf7833169e50f50
Ivan García García <ivan.garcia@altia.es>
2023-03-21 13:24:12 +0100

    first section done(description)
    change colors and font-types

--- ggbowling-front/src/modules/home/Home.tsx
+++ ggbowling-front/src/modules/home/Home.tsx
@@ -5,6 +5,40 @@ export const Home = () => {
   return (
     <div>
       <Header />
+      <main>
+        <section
+          className="first-section"
+          style={{
+            backgroundImage: `url(${process.env.PUBLIC_URL + '/img/bowling-background.jpg'})`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+            
+          }}
+        >
+          <div className="titulo">
+            <h2>GGBowling: Bolera-Restaurante</h2>
+          </div>
+          <div className="subtitulo">
+            <p>
+              GGBowling es el mejor lugar para divertirse con amigos y familia.
+              <br />
+              Ofrecemos alquileres de pistas, bolos y todos los accesorios que
+              <br />
+              necesitas para disfrutar de una noche de diversión al máximo.
+              <br />
+              Ven a pasar un rato divertido con nosotros.
+            </p>
+          </div>
+          <button className="contacta">Contactanos</button>
+        </section>
+
+        <section className="second-section"></section>
+
+        <section className="third.section"></section>
+
+        <footer></footer>
+      </main>
     </div>
   );
 };
