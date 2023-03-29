c83bdcd2a05abc7282bc8f43bb74024b431dab9f
Ivan García García <ivan.garcia@altia.es>
2023-03-29 12:06:00 +0200

    separate navbar component

---
+++ ggbowling-front/src/components/header/navBar/NavBar.tsx
@@ -0,0 +1,65 @@
+import './navBar.scss';
+
+import React, { useState } from 'react';
+import { FaAngleDown } from 'react-icons/fa';
+import { BiUserCircle } from 'react-icons/bi';
+
+export function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
+
+  function handleMenuToggle() {
+    setShowMenu(!showMenu);
+  }
+
+  return (
+    <nav className="navbar">
+      {/* Botón de menú hamburguesa */}
+      <button
+        type="button"
+        id="menu-toggle"
+        className={showMenu ? 'open' : ''}
+        onClick={handleMenuToggle}
+      >
+        <span className="sr-only">Toggle navigation</span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+        <span className="icon-bar"></span>
+      </button>
+
+      {/* Lista de navegación */}
+      <ul className={`menu ${showMenu ? 'show' : ''}`}>
+        <li>
+          <a href="/home">Inicio</a>
+        </li>
+        <li>
+          <a href="/restaurante">Restaurante</a>
+        </li>
+        <li>
+          <a href="/ofertas">Ofertas</a>
+        </li>
+        <li className="dropdown">
+          <a>
+            Bolera <FaAngleDown />
+          </a>
+          <ul className="dropdown-menu">
+            <li>
+              <a href="#">Precios</a>
+            </li>
+            <li>
+              <a href="#">Instalaciones</a>
+            </li>
+            <li>
+              <a href="#">Reservas</a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="/perfil">
+            <BiUserCircle />
+          </a>
+        </li>
+      </ul>
+    </nav>
+  );
+}
+
