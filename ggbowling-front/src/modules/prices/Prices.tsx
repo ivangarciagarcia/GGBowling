32599f48963c15c272e9d5ea8b19966d047db917
Ivan García García <ivan.garcia@altia.es>
2023-03-29 13:26:32 +0200

    prices-pages mockup code

--- ggbowling-front/src/modules/prices/Prices.tsx
+++ ggbowling-front/src/modules/prices/Prices.tsx
@@ -1,1 +1,40 @@
 import './prices.scss';
+import { Footer } from 'src/components/footer/Footer';
+import { Header } from 'src/components/header/Header';
+
+export const Prices = () => {
+  return (
+    <div>
+      <Header img={'/img/logo.png'} />
+      <main>
+        <section
+          className="info"
+          style={{
+            backgroundImage: `url(${
+              process.env.PUBLIC_URL + '/img/background-prices.jpg'
+            })`,
+            backgroundRepeat: 'no-repeat',
+            backgroundPosition: 'center',
+            backgroundSize: 'cover',
+          }}
+        >
+
+        </section>
+      </main>
+      <Footer
+        title={'GGBowling'}
+        twiLink={'https://twitter.com/Ivangg__'}
+        insLink={'https://www.instagram.com/ivangg._'}
+        linLink={'https://www.linkedin.com/in/ivan-garcia-garcia/'}
+        target={'_blank'}
+        rel={'noreferrer'}
+        street={'Dirección: Calle Falsa 123, Springfield'}
+        phone={'Teléfono: 555-1234'}
+        email={'Correo electrónico: info@bolera.com'}
+        supPage1={'Términos y condiciones'}
+        supPage2={'Política de privacidad'}
+        supPage3={'Política de cookies'}
+      />
+    </div>
+  );
+};
