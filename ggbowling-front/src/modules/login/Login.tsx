17e2225ba841502d21115eabd19072292b729c4d
Ivan García García <ivan.garcia@altia.es>
2023-04-10 10:48:08 +0200

    Login done

--- ggbowling-front/src/modules/login/Login.tsx
+++ ggbowling-front/src/modules/login/Login.tsx
@@ -6,7 +6,6 @@ import { useNavigate } from 'react-router';
 import { FRONT_BASE_URL, SERVER_BASE_URL } from 'src/config/Config';
 
 export const Login = () => {
-
   const [, setEmail] = useState('');
   const [, setPassword] = useState('');
   const [, setResponse] = useState('');
@@ -14,7 +13,6 @@ export const Login = () => {
   axios.defaults.baseURL = SERVER_BASE_URL;
   axios.defaults.headers.common['Access-Control-Allow-Origin'] = FRONT_BASE_URL;
 
-  
   const handleSubmit = async (e: any) => {
     e.preventDefault();
 
@@ -28,37 +26,45 @@ export const Login = () => {
       });
       setResponse(data); // actualiza el estado con la respuesta del servidor
       navigate('/home');
-    } catch (error) { /*TODO mensaje de error*/ }
+    } catch (error) {
+      /*TODO mensaje de error*/
+    }
   };
 
   return (
-    <form className="login-form" onSubmit={handleSubmit}>
-      <h2>Iniciar sesión</h2>
-      <div className="form-group">
-        <label htmlFor="email">Correo electrónico</label>
-        <Input
-          type={'email'}
-          id="email"
-          name="email"
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div className="form-group">
-        <label htmlFor="password">Contraseña</label>
-        <Input
-          type={'password'}
-          id="password"
-          name="password"
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <div>
-        <button type="submit">Iniciar sesión</button>
-      </div>
+    <body>
+      <main>
+        <form className="login-form" onSubmit={handleSubmit}>
+          <h2>Iniciar sesión</h2>
+          <div className="form-group">
+            <label htmlFor="email">Correo electrónico</label>
+            <Input
+              type={'email'}
+              id="email"
+              name="email"
+              onChange={(e) => setEmail(e.target.value)}
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="password">Contraseña</label>
+            <Input
+              type={'password'}
+              id="password"
+              name="password"
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <div>
+            <button type="submit">Iniciar sesión</button>
+          </div>
 
-      <div className='register'>
-        <p>No tienes cuenta? <a href={'/register'}>Registrate aquí</a></p>
-      </div>
-    </form>
+          <div className="register">
+            <p>
+              No tienes cuenta? <a href={'/register'}>Registrate aquí</a>
+            </p>
+          </div>
+        </form>
+      </main>
+    </body>
   );
 };
