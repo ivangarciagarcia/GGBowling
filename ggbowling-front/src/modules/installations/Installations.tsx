f297ad3f3cd4a6b957aa6a1ed469bd2c99f6faef
Ivan García García <ivan.garcia@altia.es>
2023-04-11 10:38:41 +0200

    Installations-page done

--- ggbowling-front/src/modules/installations/Installations.tsx
+++ ggbowling-front/src/modules/installations/Installations.tsx
@@ -1,11 +1,55 @@
+import './installations.scss';
 import { Footer } from 'src/components/footer/Footer';
 import { Header } from 'src/components/header/Header';
+import Gallery from 'react-photo-gallery';
 
 export const Installations = () => {
+  const photos = [
+    {
+      src: 'img/installations/bowling-background.jpg',
+      width: 1200,
+      height: 600,
+    },
+    {
+      src: 'img/installations/cafe-bolera.jpg',
+      width: 1600,
+      height: 900,
+    },
+    {
+      src: 'img/installations/pista.jpg',
+      width: 600,
+      height: 800,
+    },
+    {
+      src: 'img/installations/restaurant.jpg',
+      width: 2500,
+      height: 1200,
+    },
+    {
+      src: 'img/installations/restaurante2.jpg',
+      width: 318,
+      height: 159,
+    },
+    
+    {
+      src: 'img/installations/cafeteria.jpeg',
+      width: 2250,
+      height: 1000,
+    },
+    {
+      src: 'img/installations/pista2.jpg',
+      width: 1400,
+      height: 900,
+    },
+    
+
+  ];
+
   return (
     <body>
       <Header img={'/img/logo.png'} alt={'logo'} />
       <main>
+        <Gallery photos={photos} margin={10} />
       </main>
       <Footer
         title={'GGBowling'}
