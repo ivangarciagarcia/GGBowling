30b67aeabbf620d1371c4840a1c44abc054dde11
Ivan García García <ivan.garcia@altia.es>
2023-04-12 16:19:01 +0200

    component ServiceCard without <a>

--- ggbowling-front/src/components/serviceCard/ServiceCard.tsx
+++ ggbowling-front/src/components/serviceCard/ServiceCard.tsx
@@ -6,21 +6,18 @@ export interface ServiceCardProps {
   title: string;
   description: string;
   buttonText?: string;
-  href?: string;
   onClick?: () => void;
 }
 
 export const ServiceCard = (props: ServiceCardProps) => {
-  const { src, alt, title, description, buttonText, href, onClick } = props;
+  const { src, alt, title, description, buttonText, onClick } = props;
   return (
     <div className="card">
       <img src={src} alt={alt} />
       <div className="card-content">
         <h2>{title}</h2>
         <p>{description}</p>
-        <a href={href}>
-          <button onClick={onClick}>{buttonText}</button>
-        </a>
+        <button onClick={onClick}>{buttonText}</button>
       </div>
     </div>
   );
