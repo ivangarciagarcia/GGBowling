45bd1d6eb19bc043a59c5dd4ca59b377fbd60659
Ivan García García <ivan.garcia@altia.es>
2023-04-19 10:49:06 +0200

    UserModify done #TODO
    if u want see the changes in ur profile, u need to logout and login.

--- ggbowling-front/src/components/user/User.tsx
+++ ggbowling-front/src/components/user/User.tsx
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { LOGOUT_REQUEST, LOGOUT_RESPONSE } from '../../modules/login/actions';
 import { useDispatch } from 'react-redux';
 import './user.scss';
 import { useNavigate } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+import { SERVER_BASE_URL } from 'src/config/Config';
 
 export interface UserProps {
   username: string;
@@ -14,7 +16,17 @@ export interface UserProps {
   fechaNacimiento: string;
 }
 
+interface LoginState {
+  loading: boolean;
+  userInfo: any;
+  error: string | null;
+}
+
 export const User = (props: UserProps) => {
+
+  const { userInfo } = useSelector(
+    (state: { login: LoginState }) => state.login
+  );
   const {
     username,
     password,
@@ -25,6 +37,16 @@ export const User = (props: UserProps) => {
     fechaNacimiento,
   } = props;
 
+  const [newUserData, setNewUserData] = useState({
+    username,
+    password,
+    nombre,
+    primerApellido,
+    email,
+    telefono,
+    fechaNacimiento,
+  });
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -33,11 +55,44 @@ export const User = (props: UserProps) => {
     dispatch({ type: LOGOUT_RESPONSE, error: null });
     navigate('/');
   };
+
+  const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setNewUserData({
+      ...newUserData,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  const updateUser = () => {
+    const url = SERVER_BASE_URL + `/usuario/update/${userInfo.usuarioId}`;
+  
+    fetch(url, {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newUserData),
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log('Usuario actualizado:', data);
+        // Aquí puedes actualizar el estado del componente con los datos del usuario actualizado
+      })
+      .catch(error => {
+        console.error('Error al actualizar el usuario:', error);
+      });
+  };
+
   const handleModify = () => {
-    // Implementación de la función handleModify
-    // Esta función podría abrir un formulario para que el usuario pueda modificar sus datos
-    // También podría llamar a una API para guardar los cambios en el servidor
+   
     console.log('Modificar perfil');
+  
+    setNewUserData({
+      ...newUserData,
+    });
+
+    updateUser();
+    alert('Usuario actualizado correctamente');
   };
 
   return (
@@ -47,30 +102,29 @@ export const User = (props: UserProps) => {
       </div>
       <div className="user-info">
         <div className="user-info-row">
-          <span className="user-info-label">Username:</span>
-          <span>{username}</span>
+          <label className="user-info-label">Username:</label>
+          <input type="text" name="username" value={newUserData.username} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
-          <span className="user-info-label">Password:</span>
-          <span>{password}</span>
+          <label className="user-info-label">Password:</label>
+          <input type="password" name="password" value={newUserData.password} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
-          <span className="user-info-label">Name:</span>
-          <span>
-            {nombre} {primerApellido}
-          </span>
+          <label className="user-info-label">Name:</label>
+          <input type="text" name="nombre" value={newUserData.nombre} onChange={handleInputChange} />
+          <input type="text" name="primerApellido" value={newUserData.primerApellido} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
-          <span className="user-info-label">Email:</span>
-          <span>{email}</span>
+          <label className="user-info-label">Email:</label>
+          <input type="email" name="email" value={newUserData.email} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
-          <span className="user-info-label">Phone Number:</span>
-          <span>{telefono}</span>
+          <label className="user-info-label">Phone:</label>
+          <input type="number" name="telefono" value={newUserData.telefono} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
-          <span className="user-info-label">Birthdate:</span>
-          <span>{fechaNacimiento}</span>
+          <label className="user-info-label">Birthdate:</label>
+          <input type="text" name="fechaNacimiento" value={newUserData.fechaNacimiento} onChange={handleInputChange} />
         </div>
         <div className="user-info-row">
           <button className="user-btn-modify" onClick={handleModify}>
