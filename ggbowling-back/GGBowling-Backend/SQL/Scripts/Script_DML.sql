98aac711f2c7392011b52b6a5ad19ce0b61ef84e
Ivan García García <ivan.garcia@altia.es>
2023-05-02 15:50:30 +0200

    Aded examples

--- ggbowling-back/GGBowling-Backend/SQL/Scripts/Script_DML.sql
+++ ggbowling-back/GGBowling-Backend/SQL/Scripts/Script_DML.sql
@@ -2,7 +2,11 @@ use GGBowling;
 
 INSERT INTO ggbowling.usuario
 (user_name, contrasena, nombre, primer_apellido, segundo_apellido, email, telefono, fecha_nacimiento)
-VALUES('Robert24', 'Abc123..', 'Robert', 'Martinez', 'Perez', 'Robert@gmail.com', 658789632, '1994-10-12');
+VALUES('Robert24', 'Abc123..', 'Robert', 'Martinez', 'Perez', 'Robert@gmail.com', 123123123, '1994-10-12');
+
+INSERT INTO ggbowling.usuario
+(user_name, contrasena, nombre, primer_apellido, segundo_apellido, email, telefono, fecha_nacimiento)
+VALUES('Ivangg', 'qwert', 'Ivan', 'Garcia', 'Garcia', 'garciaivan200@gmail.com', 111222333, '2002-01-11');
 
 /*select * from usuario us;*/
 
@@ -55,5 +59,9 @@ VALUES(4);
 INSERT INTO ggbowling.reserva
 (usuario_id, pista_id, mesa_id, fecha, hora, personas, partidas)
 VALUES(1,1,1,'2023-01-01', '17:00', 5, 1);
+
+INSERT INTO ggbowling.reserva
+(usuario_id, pista_id, mesa_id, fecha, hora, personas, partidas)
+VALUES(2,2,2,'2023-01-11', '21:00', 8, 2);
 
 /*select * from reserva re;*/
