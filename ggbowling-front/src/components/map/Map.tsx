1e58e6bfefa68c3ed13b88668fd5f9b8c0cbfa90
Ivan GarciaGarcia <ivan.garcia.garcia@altia.es>
2023-05-25 17:47:42 +0200

    map updated

--- ggbowling-front/src/components/map/Map.tsx
+++ ggbowling-front/src/components/map/Map.tsx
@@ -1,5 +1,14 @@
+/* eslint-disable jsx-a11y/iframe-has-title */
 import './map.scss';
 
 export const Map = () => {
-  return <img src="img/home/map.jpg" alt="mapa"/>;
+  return (
+    <div className="mapa">
+      <iframe
+        className="iframe"
+        height="400"
+        src="https://www.openstreetmap.org/export/embed.html?bbox=-8.407738208770754%2C43.35479793310456%2C-8.403258919715883%2C43.356502452526534&amp;layer=mapnik"
+      ></iframe>
+    </div>
+  );
 };
